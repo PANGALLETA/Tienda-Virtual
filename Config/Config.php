e50bb1f4b87f48f1e763b9bd1a3536a930a0a6d9
PANGALLETA <julian.xvp@gmail.com>
2023-02-05 14:36:43 -0500

    1.0

--- Config/Config.php
+++ Config/Config.php
@@ -1,4 +1,21 @@
 <?php
 //define("BASE_URL", "localhost/Tienda-virtual/");
-const BASE_URL = "localhost/Tienda-virtual/";
+const BASE_URL = "http://localhost/Tienda-virtual/";
+
+//Zona horaria
+date_default_timezone_set('America/Bogota');
+
+// Datos de conexión a la Base de Datos
+const DB_HOST = "localhost";
+const DB_NAME = "db_sistema";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "charset=utf8";
+
+//Delimitadores decimal y millar Ej. 24,1989.00
+const SPD = ".";
+const SPM = ",";
+
+//Simbolo de moneda
+const SMONEY = "$";
 ?>
--- Controllers/Productos.php
+++ Controllers/Productos.php
@@ -1,15 +1,16 @@
 <?php
-class Home extends Controllers{
+class Productos extends Controllers
+{
     public function __construct()
     {
         parent::__construct();
     }
-    public function home()
+    public function Productos()
     {
-        $data['page_id'] = 1;
-        $data['page_tag'] = "Home";
-        $data['page_title'] = "Página Principal";
-        $data['page_name'] = "home";
+        $data['page_id'] = 2;
+        $data['page_tag'] = "Productos";
+        $data['page_title'] = "Productos en Venta";
+        $data['page_name'] = "Productos";
         $data['page_Content'] = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum 
         ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica 
         a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. 
@@ -17,32 +18,7 @@ class Home extends Controllers{
         igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem 
         Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem 
         Ipsum.";
-        $this->views->getView($this,"home",$data);
+        $this->views->getView($this, "productos", $data);
     }
-    /*public function insertar()
-    {
-        $data = $this->model->setUser("Julian",3225090321,"Julian@gmail.com");
-        print_r($data);
-    }
-    public function verUsuario($id)
-    {
-        $data = $this->model->getUser($id);
-        print_r($data);
-    }
-    public function actualizar()
-    {
-        $data = $this->model->updateUser(1, "Julian", 3225090321, "Julian@gmail.com");
-        print_r($data);
-    }
-    public function verUsuarios()
-    {
-        $data = $this->model->getUsers();
-        print_r($data);
-    }
-    public function eliminarUsuario($id)
-    {
-        $data = $this->model->delUser($id);
-        print_r($data);
-    }*/
 }
 ?>
