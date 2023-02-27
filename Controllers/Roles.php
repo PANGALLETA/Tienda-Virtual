e924ad5097ffb0aad644dc939bc356d1a924f7c6
PANGALLETA <julian.xvp@gmail.com>
2023-02-26 22:22:55 -0500

    3.0

---
+++ Controllers/Roles.php
@@ -0,0 +1,102 @@
+<?php
+class Roles extends Controllers{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function Roles()
+    {
+        $data['page_id'] = 3;
+        $data['page_tag'] = "Roles Usuarios - Tienda Virtual";
+        $data['page_title'] = "Roles Usuarios - <small>Tienda Virtual</small>";
+        $data['page_name'] = "Roles Usuarios";
+        $this->views->getView($this,"roles",$data);
+    }
+
+    public function getRoles()
+    {
+        $arrData = $this->model->selectRoles();
+        for ($i=0; $i < count($arrData); $i++){
+            if($arrData[$i]['status'] == 1){
+               $arrData[$i]['status'] = '<span class="badge badge-success">Activo</span>'; 
+            }
+            else{
+                $arrData[$i]['status'] = '<span class="badge badge-danger">Inactivo</span>';
+            }
+
+            $arrData[$i]['options'] = '<div class="text-center">
+            <button class="btn btn-secondary btn-sm btnPermisosRol" rl="'.$arrData[$i]['idrol'].'" title="Permisos"><i class="fas fa-key"></i></button>
+            <button class="btn btn-primary btn-sm btnEditRol" onClick="fntEditRol('.$arrData[$i]['idrol'].')" title="Editar"><i class="fas fa-pencil-alt"></i></button>
+            <button class="btn btn-danger btn-sm btnDelRol" onClick="fntDelRol('.$arrData[$i]['idrol'].')" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
+            </div>';
+        }
+        echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
+    public function getRol($idrol)
+    {
+        $intIdrol = intval(strClean($idrol));
+        if($intIdrol > 0){
+            $arrData = $this->model->selectRol($intIdrol);
+            if(empty($arrData)){
+                $arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
+            }else{
+                $arrResponse = array('status' => true, 'data' => $arrData);
+            }
+            echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+        }
+        die();
+    }
+
+    public function setRol()
+    {
+        $intIdrol = intval($_POST['idRol']);
+        $strRol = strClean($_POST['txtNombre']);
+        $strDescripcion = strClean($_POST['txtDescripcion']);
+        $intStatus= intval($_POST['listStatus']);
+
+        if($intIdrol == 0){
+            //CREAR
+            $request_rol = $this->model->insertRol($strRol,$strDescripcion,$intStatus);
+            $option = 1;
+        }else{
+            //ACTUALIZAR
+            $request_rol = $this->model->updateRol($intIdrol,$strRol,$strDescripcion,$intStatus);
+            $option = 2;
+        }
+
+        if(intval($request_rol) > 0){
+            if($option == 1){
+                $arrResponse = array('status' => true, 'msg' => 'Datos guardados correctamente.');
+            }else{
+                $arrResponse = array('status' => true, 'msg' => 'Datos Actualizados correctamente.');
+            }
+        }else if($request_rol == 'exist'){
+            $arrResponse = array('status' => false, 'msg' => '¡Atención! El Rol ya existe.');
+        }else{
+            $arrResponse = array('status' => false, 'msg' => 'No es posible almacenar los datos.');
+        }
+        echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
+    public function delRol()
+    {
+        if($_POST){
+            $intIdrol = intval($_POST['idrol']);
+            $requestDelete = $this->model->deleteRol($intIdrol);
+            if($requestDelete == 'ok')
+            {
+                $arrResponse = array('status' => true, 'msg' => 'Se ha eliminado el Rol.');
+            }else if($requestDelete == 'exist'){
+                $arrResponse = array('status' => false, 'msg' => 'No es posible eliminar un Rol asociado a usuarios.');
+            }else{
+                $arrResponse = array('status' => false, 'msg' => 'Error al eliminar el Rol');
+            }
+            echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+        }
+        die();
+    }
+}
+?>
---
+++ Views/Roles/roles.php
@@ -0,0 +1,40 @@
+<?php headerAdmin($data); ?>
+<?php NavAdmin($data); ?>
+<?php getModal('modalRoles', $data) ?>
+<main class="app-content">
+    <div class="app-title">
+        <div>
+            <h1><i class="fa fa-user-tag"></i> <?php echo $data['page_title']; ?>
+                <button class="btn btn-primary" type="button" onclick="openModal();"><i class="fa-solid fa-circle-plus"></i> Nuevo</button>
+            </h1>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+            <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>/Roles"><?php echo $data['page_title']; ?></a></li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tile">
+                <div class="tile-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover table-bordered" id="tableRoles">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nombre</th>
+                                    <th>Descripcion</th>
+                                    <th>Status</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php footerAdmin($data); ?>
